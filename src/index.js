70acff1c421c386ced7c8cab54a731056274c80f
ismailkhalid32 <ismailkhalid2407b@gmail.com>
2026-08-23 23:48:40 +0500

    port config is added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import express from "express"
+const app = express()
 import 'dotenv/config'
 import connectDB from './db/db.js'
 
+const PORT = process.env.PORT || 3000
+
 
 connectDB()
+
+app.listen('PORT',()=>{
+    console.log("server is listening Port:",PORT)
+})
