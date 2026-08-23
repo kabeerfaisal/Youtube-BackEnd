7e10a94cce70f8a59d0ff4691cc788dda2d592b4
ismailkhalid32 <ismailkhalid2407b@gmail.com>
2026-08-23 22:04:54 +0500

    added mongodb connection

---
+++ src/db/db.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+
+const connectDB = (async ()=>{
+try {
+    const connectionIntance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+    console.log(`MongoDB Connected! DB Host: ${
+        connectionIntance.connection.host
+    }`
+    )
+}catch (error) {
+    console.error("Mongodb not Connected Error",error)
+    process.exit(1)
+}
+})
+
+export default connectDB
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import express from "express"
+import 'dotenv/config'
+import connectDB from './db/db.js'
+
+
+connectDB()
